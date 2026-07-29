eb7f5fcc4f8dd62ad8e6f641d64dd6d9aac23d6e
Gabriel Berneri <gabyberneri.gb@gmail.com>
2026-07-28 21:34:55 -0300

    Agregar WhatsApp a la pagina de contacto

--- src/views/paginas/contacto.php
+++ src/views/paginas/contacto.php
@@ -8,6 +8,11 @@
 <section class="pagina-contacto">
     <div class="contenedor">
         <div class="contacto-tarjetas">
+            <a href="https://wa.me/5491158438800" target="_blank" rel="noopener" class="contacto-tarjeta">
+                <span class="contacto-icono">💬</span>
+                <span class="contacto-titulo">WhatsApp</span>
+                <span class="contacto-dato">11 5843-8800</span>
+            </a>
             <a href="https://www.instagram.com/eltesorodelpique/" target="_blank" rel="noopener" class="contacto-tarjeta">
                 <span class="contacto-icono">📷</span>
                 <span class="contacto-titulo">Instagram</span>
